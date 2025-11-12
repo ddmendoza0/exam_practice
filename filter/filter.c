8e8b5b4cd6c5e43067f810567ed2efebed63453a
David Mendoza <ddmendozafernandez@gmail.com>
2025-11-12 18:13:20 +0100

    filter

---
+++ filter/filter.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+
+
+
+
+
+void	filter(char *pajar, char *aguja)
+{
+
+
+
+}
+
+
+char	*read_all(void)
+{
+
+
+}
+
+
+int	main(int argc, char *argv[])
+{
+	if(argc != 1 || !argv[1] || argv[1} == `\0')
+		return(1);
+	char	*pajar;
+
+
+
+}
