3890bba7c9b2dc378e4e10966df86e84c31d228c
David Mendoza <ddmendozafernandez@gmail.com>
2026-03-08 09:42:46 +0100

    ft_popen practice 100

---
+++ rank04/level1/ft_popen/ft_popen07.c
@@ -0,0 +1,48 @@
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int	ft_popen(const char *file, char *const argv[], char type)
+{
+	int	fds[2];
+	pid_t	pid;
+
+	if (!file || !argv || (type != 'r' && type != 'w'))
+		return (-1);
+
+	if (pipe(fds) == -1)
+		return (-1);
+
+	pid = fork();
+	if (pid == -1)
+	{
+		close(fds[0]);
+		close(fds[1]);
+		return (-1);
+	}
+
+	if (pid == 0)
+	{
+		if (type == 'r')
+			dup2(fds[1], STDOUT_FILENO);
+		else
+			dup2(fds[0], STDIN_FILENO);
+		close(fds[0]);
+		close(fds[1]);
+		execvp(file, argv);
+		exit(1);
+	}
+
+	if (type == 'r')
+	{
+		close(fds[1]);
+		return (fds[0]);
+	}
+	else
+	{
+		close(fds[0]);
+		return (fds[1]);
+	}
+
+
+}
